934e8170fc4cd75f67e3f29f3efa246371afeb29
nicolaparutto <nicolaparutto2003@gmail.com>
2025-01-22 18:07:44 +0100

    feat: got the data from the api and printed the posts on the page Base Exercise Completed

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,57 +1,50 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
+
 function Blog() {
+
+   //API default endpoint:
+   const endpointApi = "http://localhost:3000";
+
+   //Chiamata API:
+   const fetchPosts = () => {
+      axios.get(`${endpointApi}/posts`)
+         .then(res => {
+            setPostsList(res.data)
+         })
+         .catch(error => {
+            console.error("Errore durante la chiamata al server", error)
+         })
+   }
+
+   //Variabile di stato che contiene l'array dei post dell'API:
+   const [postsList, setPostsList] = useState([])
+
+   useEffect(() => {
+      fetchPosts()
+   }, [])
+
    return (
       <>
          <section className="container">
+            <h1>Lista dei post</h1>
             <div className="card-container">
                <div className="card-row">
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
+                  {postsList.map(post => (
+                     <div key={post.id} className="card">
+                        <div className='card-content'>
+                           <div className="card-img">
+                              <img src={post.image} alt="" />
+                           </div>
+                           <div className="card-text">
+                              <h2>{post.title}</h2>
+                              <p>{post.content}</p>
+                              <p><strong>Tags: </strong>{post.tags.join(", ")}</p>
+                           </div>
+                        </div>
                      </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
-                     </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
-                     </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
-                     </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
-                     </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
-                  <div className="card">
-                     <div className="card-img">
-                        immagine
-                     </div>
-                     <div className="card-text">
-                        testo
-                     </div>
-                  </div>
+                  ))}
                </div>
             </div>
          </section>
